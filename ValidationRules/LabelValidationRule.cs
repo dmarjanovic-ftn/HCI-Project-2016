321bb563a1111fdb539c2a96f55f007a4d021cad
dmarjanovic94 <dmarjanovic94@gmail.com>
2016-04-23 22:50:55 +0200

    Placeholder and ValidationRules example added.

---
+++ ValidationRules/LabelValidationRule.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace HCI_2016_Project.ValidationRules
+{
+    // Pravilo kojim provjeravamo da li je uneseno polje i duzinu unesenog polja
+    public class LengthValidationRule : ValidationRule
+    {
+        private static int MIN_LENGTH = 8;
+        private static int MAX_LENGTH = 20;
+
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            try
+            {
+                var text = value as string;
+                if (0 == text.Length)
+                {
+                    return new ValidationResult(false, "Oznaka manifestacije je obavezna.");
+                }
+                else if (text.Length < MIN_LENGTH)
+                {
+                    return new ValidationResult(false, "Minimalna dužina polja je " + MIN_LENGTH + " karaktera.");
+                }
+                else if (text.Length > MAX_LENGTH)
+                {
+                    return new ValidationResult(false, "Maksimalna dužina polja je " + MAX_LENGTH + " karaktera.");
+                }
+                else
+                {
+                    return new ValidationResult(true, null);
+                }
+            }
+            catch
+            {
+                return new ValidationResult(false, "Desila se nepredviđena greška.");
+            }
+        }
+    }
+
+    // Pravilo kojim provjeravamo sadrzaj koji je unesen u polje
+    public class ContentValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            try
+            {
+                var text = value as string;
+                if (0 == text.Length)
+                {
+                    return new ValidationResult(false, "Oznaka manifestacije je obavezna.");
+                }
+                else if (text.Length < 5)
+                {
+                    return new ValidationResult(false, "Minimalna dužina polja je karaktera.");
+                }
+                else if (text.Length > 10)
+                {
+                    return new ValidationResult(false, "Maksimalna dužina polja je karaktera.");
+                }
+                else
+                {
+                    return new ValidationResult(true, null);
+                }
+            }
+            catch
+            {
+                return new ValidationResult(false, "Desila se nepredviđena greška.");
+            }
+        }
+    }
+
+    // Pravilo kojim provjeravamo da li je uneseno polje jedinstveno
+    public class UniqueValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            try
+            {
+                var text = value as string;
+                if ("ABCDEFGH123" == text)
+                {
+                    return new ValidationResult(false, "Unesena oznaka već postoji. Polje mora biti jedinstveno.");
+                }
+                else
+                {
+                    return new ValidationResult(true, null);
+                }
+            }
+            catch
+            {
+                return new ValidationResult(false, "Desila se nepredviđena greška.");
+            }
+        }
+    }
+}
