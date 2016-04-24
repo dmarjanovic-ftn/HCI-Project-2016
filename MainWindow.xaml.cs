dd4f288f000e4dfe4008102f8fc373af299daf1b
dmarjanovic94 <dmarjanovic94@gmail.com>
2016-04-24 11:25:35 +0200

    Minor Toolbar design changes.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -80,5 +80,21 @@ namespace HCI_2016_Project
         {
 
         }
+
+        private void ToolBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            ToolBar toolBar = sender as ToolBar;
+            var overflowGrid = toolBar.Template.FindName("OverflowGrid", toolBar) as FrameworkElement;
+            if (overflowGrid != null)
+            {
+                overflowGrid.Visibility = Visibility.Collapsed;
+            }
+
+            var mainPanelBorder = toolBar.Template.FindName("MainPanelBorder", toolBar) as FrameworkElement;
+            if (mainPanelBorder != null)
+            {
+                mainPanelBorder.Margin = new Thickness(0);
+            }
+        }
     }
 }
