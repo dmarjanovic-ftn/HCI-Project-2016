2bc8525386520eb8619786a02fcc88ae9d9172f3
dmarjanovic94 <dmarjanovic94@gmail.com>
2016-05-19 02:31:01 +0200

    Added Drag&Drop of Manifestation Icon to Manifestations' Canvas.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,11 +25,17 @@ namespace HCI_2016_Project
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int ICON_SIZE  = 20;
+        private const int OFFSET     = ICON_SIZE / 2;
+
         private ViewModel vm;
+
+        Point start = new Point();
 
         public class ViewModel
         {
             public ObservableCollection<Manifestation> Manifestations { get; set; }
+            public ObservableCollection<Manifestation> DroppedManifestations { get; set; }
         }
 
         public MainWindow()
@@ -117,6 +123,8 @@ namespace HCI_2016_Project
 
             vm = new ViewModel();
             vm.Manifestations = new ObservableCollection<Manifestation>();
+            vm.DroppedManifestations = new ObservableCollection<Manifestation>();
+
             foreach (Manifestation manifestation in AppData.GetInstance().Manifestations)
             {
                 vm.Manifestations.Add(manifestation);
@@ -124,6 +132,84 @@ namespace HCI_2016_Project
 
             Console.WriteLine(vm.Manifestations.Count);
             this.DataContext = vm;
+        }
+
+        // Click on Manifestation on left sidebar
+        private void ManifestationItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            start = e.GetPosition(null);
+
+            StackPanel test = sender as StackPanel;
+            
+            Console.WriteLine(test.Tag);
+        }
+
+        // Move Manifestation Item
+        private void ManifestationItem_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Console.WriteLine("moving...");
+
+            StackPanel manifestationPanel = sender as StackPanel;
+            Manifestation dataObject = null;
+            foreach (Manifestation manifestation in vm.Manifestations)
+            {
+                if ((string) manifestationPanel.Tag == manifestation.Label)
+                {
+                    dataObject = manifestation;
+                    break;
+                }
+            }
+
+            // Initialize the drag & drop operation
+            DataObject data = new DataObject("myFormat", dataObject);
+            DragDrop.DoDragDrop(manifestationPanel, data, DragDropEffects.Move);
+        }
+
+        // Drag Manifestation Item into Manifestations' Map
+        private void ManifestationsMap_DragEnter(object sender, DragEventArgs e)
+        {
+            Console.WriteLine("test");
+        }
+
+        // Drop Manifestation Item
+        private void ManifestationsMap_Drop(object sender, DragEventArgs e)
+        {
+            Console.WriteLine("dropped");
+
+            Point dropPosition = e.GetPosition(ManifestationsMap);
+
+            if (e.Data.GetDataPresent("myFormat"))
+            {
+                Manifestation manifestation = e.Data.GetData("myFormat") as Manifestation;
+                vm.Manifestations.Remove(manifestation);
+                Console.WriteLine(manifestation.Name);
+
+                Canvas canvas = this.ManifestationsMap;
+
+                Image ManifestationIcon = new Image
+                {
+                    Width  = ICON_SIZE,
+                    Height = ICON_SIZE,
+                    //Name = manifestation.Name,
+                    Source = new BitmapImage(new Uri(manifestation.IconSrc, UriKind.Absolute)),
+                };
+                /*BitmapImage icon = new BitmapImage(new Uri(manifestation.IconSrc, UriKind.Absolute));
+                Image ManifestationIcon*/
+
+                /*Ellipse r = new Ellipse();
+                r.Height=10;
+                r.Width=10;
+                r.Stroke =Brushes.Black;*/
+
+                canvas.Children.Add(ManifestationIcon);
+
+                Canvas.SetLeft(ManifestationIcon, dropPosition.X - OFFSET);
+                Canvas.SetTop(ManifestationIcon, dropPosition.Y - OFFSET);
+
+                /*root.Children.Add(e);
+                Y +=10;*/
+                //Studenti2.Add(student);
+            }
         }
     }
 }
