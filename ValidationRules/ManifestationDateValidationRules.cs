4ac088eafb42554a070151e122d21e3845b05716
dmarjanovic94 <dmarjanovic94@gmail.com>
2016-05-27 16:24:20 +0200

    Validation Rules minor changes.

---
+++ ValidationRules/ManifestationDateValidationRules.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace HCI_2016_Project.ValidationRules
+{
+    public class MinDateValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            Console.WriteLine("Test");
+
+            DateTime manifestationDate = (DateTime) value;
+
+            return new ValidationResult(manifestationDate > DateTime.Now, "Datum ne smije biti stariji od današnjeg.");
+        }
+    }
+}
