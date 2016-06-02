57820a800208e5639beac0ba736676cc97feb234
dmarjanovic94 <dmarjanovic94@gmail.com>
2016-06-03 00:06:41 +0200

    Add Manifestation button disabled until Validation rules are wrong.

---
+++ Binders/LengthToBooleanConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HCI_2016_Project.Binders
+{
+    public class LengthToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Int32 length = (Int32)value;
+            Console.WriteLine(length);
+            if (length == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
